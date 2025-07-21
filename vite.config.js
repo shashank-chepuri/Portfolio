9d2f161f1d6b875be9f644563bbe428880d097c7
Shashank <shashankchepuri55@gmail.com>
2025-07-21 20:03:49 +0530

    Cleaned up build and removed gh-pages cache

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
+  base: "/Portfolio/", // This is crucial for GitHub Pages
   plugins: [react()],
   css: {
     modules: {
-      localsConvention: "camelCase",
-       
+      localsConvention: "camelCase", // Optional, fine to include
     },
   },
 });
