f5bcd8408ef4f7e0890c6d1d7919e4fdcdfcfdcb
Shashank <shashankchepuri55@gmail.com>
2025-07-21 19:35:29 +0530

    Fix vite config and ready for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Portfolio/",
   plugins: [react()],
   css: {
     modules: {
       localsConvention: "camelCase",
+       
     },
   },
 });
